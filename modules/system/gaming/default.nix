9766df2d825ec00407db23a7e7e9b26dbe1576b7
Nazarii Palahnii <nazariipalahnii@gmail.com>
2025-04-28 00:01:28 +0200

    lil update

--- modules/system/gaming/default.nix
+++ modules/system/gaming/default.nix
@@ -1,5 +1,10 @@
-_: {
-  imports = [
-    ./steam.nix
-  ];
+{ pkgs, ... }:
+{
+  programs = {
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+    };
+    gamemode.enable = true;
+  };
 }
